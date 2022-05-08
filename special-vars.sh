24dba6432cbd161d73618d1c42abc84b9963d2ac
Vikram691 <dasaramvikram@gmail.com>
2022-05-08 13:26:56 +0530

    addeded

--- special-vars.sh
+++ special-vars.sh
@@ -2,6 +2,10 @@
 
 # special characters are $0-$n,$@,$#,$*
 
-
+echo Script name = $0
 echo First Arguement = $1
 echo Second Arguement = $2
+
+echo All Arguements = $*
+
+echo No of Arguements = $#
