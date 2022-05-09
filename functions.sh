2a2c44e96d29d0ce926938981933cabf946057cb
Vikram691 <dasaramvikram@gmail.com>
2022-05-09 11:32:42 +0530

    addeded

---
+++ functions.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+#if we assign a name to set of data is called variable
+# if we assign a name to set of commands is called Functions
+
+# Declare a function
+
+# syntax:
+      #FunctionName(){
+
+        # commands
+      #}
+
+      sample(){
+
+        echo I am a sample function
+        }
+
+        ## Main program
+
+        sample
+
+## Function always be declared first and then we need to call them later in main program
