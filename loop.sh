5ae75f2b67b926eb33014d58906419fb30fc2734
Vikram691 <dasaramvikram@gmail.com>
2022-05-28 12:29:56 +0530

    12

--- loop.sh
+++ loop.sh
@@ -8,12 +8,12 @@
 for fruit in apple banana orange
 do
   echo Fruit Name = $fruit
-  sleep1
+  sleep 1
   done
 
 # while loop, if we want to run loop for number of times based on expression then go for while loop
 a=10
 while [ $a -gt 0 ]; do
   echo $a
-  a=$(($a-1))
+  //a=$(($a-1))
   done
