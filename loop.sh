5515efaf289d549648f5ed6f510029632aa2e8e0
Vikram691 <dasaramvikram@gmail.com>
2022-05-28 12:28:59 +0530

    12

---
+++ loop.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# two loops for and while
+# But when to go with which loop
+
+# for loop, if our inputs are list of values/strings then we go with for loop
+
+for fruit in apple banana orange
+do
+  echo Fruit Name = $fruit
+  sleep1
+  done
+
+# while loop, if we want to run loop for number of times based on expression then go for while loop
+a=10
+while [ $a -gt 0 ]; do
+  echo $a
+  a=$(($a-1))
+  done
